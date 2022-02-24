9906d4bfcc8d117b6b6bb52d12f4b47c9e138d8e
Monish_code <moneeshchoudhary@gmail,com>
2022-02-24 19:53:29 +0530

    new change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit{
   ob: Observable<any> | undefined;
 
   constructor() {
+    console.log("HAPPY PERSON");
     // create the observable stream
     this.ob = new Observable<any>(
       obs => {
